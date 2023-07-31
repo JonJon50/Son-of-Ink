bf1b2130bb2c6ef9799404fe717d80b4f2a60aad
John Hagens <ginuwine104@gmail.com>
2023-07-31 15:43:07 -0400

    Artist.jsx: update containers pushing up for pr

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fb48dbcccc72e072"; }
+/******/ 		__webpack_require__.h = function() { return "ae5f0b6bd38c528b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
