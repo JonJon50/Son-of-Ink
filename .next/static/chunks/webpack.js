c63b5fadde7bc3f21a5aab527e86e12b1459eca2
John Hagens <ginuwine104@gmail.com>
2023-08-30 18:00:47 -0400

    artistName: background sizing

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "00d88870c0f24329"; }
+/******/ 		__webpack_require__.h = function() { return "ef61ca98d247f6a6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/[artistName].js
+++ src/pages/[artistName].js
@@ -18,7 +18,7 @@ const ArtistGalleryPage = () => {
   let backgroundStyle = {}; // Define background styles based on artistName
   if (selectedArtist.name === 'Theron') {
     backgroundStyle = {
-      background: `url("https://img.freepik.com/free-photo/tattoo-salon-process-tattoo-girl-stuffed-tattoo-process-stuffing-tattoo-body-hands-close-up_1321-3001.jpg?w=1800&t=st=1693420168~exp=1693420768~hmac=56663410eb92bd76b6980e489222e461de3a884e4bae83278a38772100edc9dc")`,
+      background: `url("${selectedArtist.imageUrl}")`, // Use the imageUrl from artistsData
       backgroundSize: 'cover',
       backgroundPosition: 'center top', // Adjust the background position
       backgroundRepeat: 'no-repeat',
@@ -27,9 +27,9 @@ const ArtistGalleryPage = () => {
     };
   } else if (selectedArtist.name === 'Jonny') {
     backgroundStyle = {
-      background: `url("https://img.freepik.com/free-photo/man-doing-tattoo-tattoo-salon_1157-24948.jpg?w=1800&t=st=1693421685~exp=1693422285~hmac=0a579f5faddebd06bbef97a21db2ad00e88bcb057dc37706cc8bfd142a0989f5")`,
+      background: `url("${selectedArtist.imageUrl}")`, // Use the imageUrl from artistsData
       backgroundSize: 'cover',
-      backgroundPosition: 'center top', // Adjust the background position
+      backgroundPosition: 'center', // Adjust the background position for pic gallery
       backgroundRepeat: 'no-repeat',
       width: '100vw',
       height: '60vh',
@@ -41,18 +41,20 @@ const ArtistGalleryPage = () => {
       <div className="background-image" style={backgroundStyle}></div>
       <div className="gallery-content" style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', maxWidth: '1200px', width: '100%', padding: '20px' }}>
         <h2>{selectedArtist.name}'s Gallery</h2>
-        <div>
-          {selectedArtist.galleryImages.map((image, index) => (
-            <img
-              key={index}
-              src={image.url}
-              alt={`Gallery Image ${index}`}
-              style={{ width: '100%', maxWidth: '300px', marginBottom: '20px' }}
-            />
-          ))}
+        <div className={styles.galleryContainer}>
+          <div className={styles.galleryImages}>
+            {selectedArtist.galleryImages.map((image, index) => (
+              <img
+                key={index}
+                src={image.url}
+                alt={`Gallery Image ${index}`}
+                className={styles.galleryImage}
+              />
+            ))}
+          </div>
         </div>
         <Link href="/booking">
-        <button className={`${styles['round-button']} ${styles['text-overlay']}`} style={{ padding: '10px 20px' }}>
+          <button className={`${styles['round-button']} ${styles['text-overlay']}`} style={{ padding: '10px 20px' }}>
             Book Now
           </button>
         </Link>
