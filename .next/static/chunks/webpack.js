e20028129262889962433347771f7291f5b7df86
John Hagens <ginuwine104@gmail.com>
2023-10-09 16:32:04 -0400

    css added

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "fa6f54f2770fd702"; }
+/******/ 		__webpack_require__.h = function() { return "45493e7fc1359fc7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
