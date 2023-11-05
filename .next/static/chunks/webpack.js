50fba646eb94ed61c75d1c0670867ef9a1d6ce0a
NEO <qj82189@gmail.com>
2023-11-05 01:03:28 -0400

    added email capability

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b4fa163aa99e228"; }
+/******/ 		__webpack_require__.h = function() { return "89fd8f508d91c3e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
