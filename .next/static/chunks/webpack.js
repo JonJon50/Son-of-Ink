ecca5a069f3a77f12154c56bf9aa6e051691c8dd
Quentin Jones <qj82189@gmail.com>
2023-11-21 17:05:48 -0500

    some style changes and removed location

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4d09d4761c382d21"; }
+/******/ 		__webpack_require__.h = function() { return "09c2df0905bfa14c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Map from "../components/map/Map";
+import './location.module.css'
 
 const Location = () => {
   return (
-    <div className="">
-      <div>
+    <div className="whole-container">
+      <div className="direction-container">
         <p>
           <span>Location</span>
         </p>
       </div>
-      <div>
+      <div className="map-container">
         <Map />
       </div>
     </div>
