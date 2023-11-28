184e52a920eacd7479b133283b8544cc93750b42
John Hagens <ginuwine104@gmail.com>
2023-11-27 21:07:25 -0500

    location: page update

--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import Map from "../components/map/Map";
-import './location.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAddressCard, faPhone, faClock } from '@fortawesome/free-solid-svg-icons';
+import styles from './location.module.css';
+
 
 const Location = () => {
+  const [isContactVisible, setContactVisible] = useState(false);
+
+  const toggleContactInfo = () => {
+    setContactVisible(!isContactVisible);
+  };
+
   return (
-    <div className="whole-container">
-      <div className="direction-container">
-        <p>
-          <span>Location</span>
-        </p>
+    <div className={styles['whole-container']}>
+      <div className={styles['location-header']}>
+        <span>Location</span>
       </div>
-      <div className="map-container">
-        <Map />
+      <div className={styles['content-container']}>
+        <div className={styles['contact-container']}>
+        <div className={styles['contact-info']}>
+  <FontAwesomeIcon icon={faAddressCard} />
+  <h5>ADDRESS</h5>  
+  <p>2170 West State Road 434<br />Suite 190<br />Longwood, FL 32779</p>
+</div>
+<div className={styles['contact-info']}>
+  <FontAwesomeIcon icon={faPhone} />
+  <h5>PHONE</h5>
+  <p>407-990-1921</p>
+</div>
+<div className={styles['contact-info']}>
+  <FontAwesomeIcon icon={faClock} />
+  <h5>OPENING HOURS</h5>  
+  <p>Mon – Fri, 8:00am – 4:30pm<br />
+     Closed on Weekends, Thanksgiving, Christmas, New Years, Memorial Day, 4th of July, Labor Day</p>
+</div>
+        </div>
+        <div className={styles['map-container']}>
+          <Map />
+        </div>
       </div>
     </div>
   );
 };
 
 export default Location;
+
