9a1095903e3fdf2ac16022c3a24cbe1313e7b8cf
John Hagens <ginuwine104@gmail.com>
2023-11-28 00:29:27 -0500

    location: container

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c3d1d62c9520c280"; }
+/******/ 		__webpack_require__.h = function() { return "593c8ce9f024ae8d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/location.jsx
+++ src/pages/location.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import Map from "../components/map/Map";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faAddressCard, faPhone, faClock } from '@fortawesome/free-solid-svg-icons';
-import styles from './location.module.css';
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faAddressCard,
+  faPhone,
+  faClock,
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
+import styles from "./location.module.css";
 
 const Location = () => {
   const [isContactVisible, setContactVisible] = useState(false);
@@ -13,30 +18,49 @@ const Location = () => {
   };
 
   return (
-    <div className={styles['whole-container']}>
-      <div className={styles['location-header']}>
+    <div className={styles["whole-container"]}>
+      <div className={styles["location-header"]}>
         <span>Location</span>
       </div>
-      <div className={styles['content-container']}>
-        <div className={styles['contact-container']}>
-        <div className={styles['contact-info']}>
-  <FontAwesomeIcon icon={faAddressCard} />
-  <h5>ADDRESS</h5>  
-  <p>2170 West State Road 434<br />Suite 190<br />Longwood, FL 32779</p>
-</div>
-<div className={styles['contact-info']}>
-  <FontAwesomeIcon icon={faPhone} />
-  <h5>PHONE</h5>
-  <p>407-990-1921</p>
-</div>
-<div className={styles['contact-info']}>
-  <FontAwesomeIcon icon={faClock} />
-  <h5>OPENING HOURS</h5>  
-  <p>Mon – Fri, 8:00am – 4:30pm<br />
-     Closed on Weekends, Thanksgiving, Christmas, New Years, Memorial Day, 4th of July, Labor Day</p>
-</div>
+      <div className={styles["content-container"]}>
+        <div
+          className={`${styles["contact-container"]} ${
+            isContactVisible ? styles.visible : ""
+          }`}
+        >
+          <div className={styles["contact-info"]}>
+            <FontAwesomeIcon icon={faAddressCard} />
+            <h5>ADDRESS</h5>
+            <p>
+              2170 West State Road 434
+              <br />
+              Suite 190
+              <br />
+              Longwood, FL 32779
+            </p>
+          </div>
+          <div className={styles["contact-info"]}>
+            <FontAwesomeIcon icon={faPhone} />
+            <h5>PHONE</h5>
+            <p>407-990-1921</p>
+          </div>
+          <div className={styles["contact-info"]}>
+            <FontAwesomeIcon icon={faClock} />
+            <h5>OPENING HOURS</h5>
+            <p>
+              Mon – Fri, 8:00am – 4:30pm
+              <br />
+              Closed on Weekends, Thanksgiving, Christmas, New Years, Memorial
+              Day, 4th of July, Labor Day
+            </p>
+          </div>
+          <div className={styles["toggle-arrow"]} onClick={toggleContactInfo}>
+          <FontAwesomeIcon
+  icon={isContactVisible ? faChevronLeft : faChevronRight}
+/>
+          </div>
         </div>
-        <div className={styles['map-container']}>
+        <div className={styles["map-container"]}>
           <Map />
         </div>
       </div>
@@ -45,4 +69,3 @@ const Location = () => {
 };
 
 export default Location;
-
