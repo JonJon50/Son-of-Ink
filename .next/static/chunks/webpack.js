c7281644535b4faa329d06f81f5f96eff919d67e
John Hagens <ginuwine104@gmail.com>
2023-11-29 10:15:51 -0500

    map letters white & center title

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4e4adc0e3f4e551e"; }
+/******/ 		__webpack_require__.h = function() { return "774ec70fda3cd4b0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
