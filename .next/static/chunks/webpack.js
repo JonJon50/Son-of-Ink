f90eb3f8dc6268c5642d950eb34b3c1f270d2a22
John Hagens <ginuwine104@gmail.com>
2023-11-30 13:58:28 -0500

    Home: added blue video about section

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad3e16cff9727bbb"; }
+/******/ 		__webpack_require__.h = function() { return "5288808a9afc4c0b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -4,15 +4,19 @@ import styles from "./Home.module.css";
 import Link from "next/link";
 import { motion } from "framer-motion";
 import CountUp from "react-countup";
-import { useInView } from 'react-intersection-observer';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHourglass, faHandshake, faUsers, faChalkboardTeacher } from '@fortawesome/free-solid-svg-icons';
-
+import { useInView } from "react-intersection-observer";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHourglass,
+  faHandshake,
+  faUsers,
+  faChalkboardTeacher,
+} from "@fortawesome/free-solid-svg-icons";
 
 const StatItem = ({ end, suffix, title, icon }) => {
   const { ref, inView } = useInView({
     triggerOnce: true, // The animation will only run once when the component comes into view for the first time
-    threshold: 0.3,    // The component starts the animation when 30% of it is visible within the viewport
+    threshold: 0.3, // The component starts the animation when 30% of it is visible within the viewport
   });
 
   return (
@@ -65,10 +69,47 @@ const Home = () => {
       </div>
       {/* Statistics section */}
       <div className={styles.statisticsSection}>
-        <StatItem end={10} suffix="+" title="Years in the Industry" icon={faHourglass} />
+        <StatItem
+          end={10}
+          suffix="+"
+          title="Years in the Industry"
+          icon={faHourglass}
+        />
         <StatItem end={3192} title="Client Relationships" icon={faHandshake} />
         <StatItem end={204070} title="Tattoos Done" icon={faUsers} />
         <StatItem end={6173} title="Members" icon={faChalkboardTeacher} />
+      </div>
+      {/* New video section with overlay */}
+      <div className={styles.videoOverlaySection}>
+        {/* Use the URL directly in the src attribute */}
+        <video
+          src="https://www.sentinelone.com/wp-content/uploads/2019/11/S1_Website_Section4_Background_Waves-1.mp4%22%20type=%22video/mp4%22%20src=%22https://www.sentinelone.com/wp-content/uploads/2019/11/S1_Website_Section4_Background_Waves-1.mp4"
+          autoPlay
+          loop
+          muted
+          className={styles.overlayVideo}
+        />
+        <div className={styles.overlayContent}>
+          <motion.h1
+            className={styles.mainTitle}
+            variants={fadeInUp}
+            initial="initial"
+            animate="animate"
+            transition={{ duration: 1.0, delay: 0.5 }}
+          >
+            Listening to your vision, <br /> and bringing it to <br /> life.
+          </motion.h1>
+          <div className={styles.sideBySideParagraphs}>
+            <div className={styles.paragraphSection}>
+              <h2 className={styles.subtitle}>Who we are</h2>
+              <p>I'm Theron who has a passion for art. Making client ideas come to life is the best satisfaction along with a smile.  </p>
+            </div>
+            <div className={styles.paragraphSection}>
+              <h2 className={styles.subtitle}>What we do</h2>
+              <p>Artists from DMV area with 10 years + experience   </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
