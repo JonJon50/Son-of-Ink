c6a2f9a2398eb83d6571f07db6431db9305d4d9e
John Hagens <ginuwine104@gmail.com>
2023-12-05 22:02:28 -0500

    prep heal download file

--- src/components/document/Doc.jsx
+++ src/components/document/Doc.jsx
@@ -6,7 +6,7 @@ const Doc = () => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Preparing For Your Tattoo</h1>
-      <p className={styles.text}>If you are going to be tattooed in an area where you have a lot of body hair, Please shave 48 hours before. Make sure to lotion the area for one week leading up to your tattoo appointment, also during that week make sure to drink plenty of water or coconut water so you are well hydrated. these make for a smooth tattooing exspeirence.
+      <p className={styles.text}>If you are going to be tattooed in an area where you have a lot of body hair, please shave 48 hours before. Make sure to lotion the area for one week leading up to your tattoo appointment, also during that week make sure to drink plenty of water or coconut water so you are well hydrated. these make for a smooth tattooing exspeirence.
       < br/>
       < br/>
 Try to get a good restful sleep the night before your appointment. We don't want you exhausted. Do not party the night before. No Alcohol, the night before and Day of your tattoo keep the partying to a minimum.
@@ -19,9 +19,15 @@ If your tattoo is going to take 2 hours or more it is good to bring some fruit j
 < br/>
 < br/>
 Wear something appropriate. Remember what part of your body we need access to. Darker colors are better. Don't wear something brand new and/or white, as we are likely to get some ink on it. Avoid tight clothing.</p>
+< br/>
+< br/>
+<p className={styles.download}> Click Here... </p>
       <a href="/Assets/ConsentForm.jpeg" download className={styles.downloadLink}>
         Consent and Release Form
       </a>
+      <p className={styles.description}>
+        Before we proceed with your tattoo, it is essential to review and sign our Consent and Release Form. This document ensures that you understand and agree to the terms of service, including acknowledging the risks associated with tattooing and confirming your consent to the procedure.
+      </p>
     </div>
   );
 };
