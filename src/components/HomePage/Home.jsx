3ef451bea0a16b9ffdebec9deab928f9980a762c
John <ginuwine104@gmail.com>
2024-09-18 13:48:08 -0400

    videoupdate

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import videoBg from "public/static/videos/Tattoo Video.mp4";
 import styles from "./Home.module.css";
 import Link from "next/link";
 import { motion } from "framer-motion";
@@ -13,10 +12,11 @@ import {
   faChalkboardTeacher,
 } from "@fortawesome/free-solid-svg-icons";
 
+// Component to display the stats
 const StatItem = ({ end, suffix, title, icon }) => {
   const { ref, inView } = useInView({
-    triggerOnce: true, // The animation will only run once when the component comes into view for the first time
-    threshold: 0.3, // The component starts the animation when 30% of it is visible within the viewport
+    triggerOnce: true,
+    threshold: 0.3, // Animation triggers when 30% of the element is in view
   });
 
   return (
@@ -32,19 +32,26 @@ const StatItem = ({ end, suffix, title, icon }) => {
   );
 };
 
+// Main Home component
 const Home = () => {
   const fadeInUp = {
     initial: { y: 90, opacity: 0 },
     animate: { y: 0, opacity: 1 },
   };
 
-  
   return (
     <div className={styles.home}>
       <div className={styles.videoContainer}>
-        <video src={videoBg} autoPlay loop muted  playsInline className={styles.video} />
+        <video
+          src="/static/videos/Tattoo-Videos.mp4"  // Direct file path from public directory
+          autoPlay
+          loop
+          muted
+          playsInline
+          className={styles.video}
+        />
       </div>
-      {/* <div className={styles.mobileBackground}></div> */}
+
       <motion.h5
         className={`${styles["text-overlay"]} ${styles["customFont"]}`}
         variants={fadeInUp}
@@ -68,6 +75,7 @@ const Home = () => {
           </motion.button>
         </Link>
       </div>
+
       {/* Statistics section */}
       <div className={styles.statisticsSection}>
         <StatItem
@@ -80,15 +88,15 @@ const Home = () => {
         <StatItem end={204070} title="Tattoos Done" icon={faUsers} />
         <StatItem end={6173} title="Members" icon={faChalkboardTeacher} />
       </div>
-      {/* New video section with overlay */}
+
+      {/* Video section with overlay */}
       <div className={styles.videoOverlaySection}>
         <video
-          // src="https://www.sentinelone.com/wp-content/uploads/2019/11/S1_Website_Section4_Background_Waves-1.mp4%22%20type=%22video/mp4%22%20src=%22https://www.sentinelone.com/wp-content/uploads/2019/11/S1_Website_Section4_Background_Waves-1.mp4"
           autoPlay
           loop
           muted
           preload="auto"
-          poster="./Assets/HandsTattooing.jpeg" // Add a poster image
+          poster="/Assets/HandsTattooing.jpeg" // Poster image before the video loads
           className={styles.overlayVideo}
         />
         <div className={styles.overlayContent}>
@@ -99,7 +107,7 @@ const Home = () => {
             animate="animate"
             transition={{ duration: 1.0, delay: 0.5 }}
           >
-            Imagine your vision, <br /> and bring it to <br /> life.
+            Imagine your vision, and <br /> bring it to life.
           </motion.h1>
           <div className={styles.sideBySideParagraphs}>
             <div className={styles.paragraphSection}>
@@ -113,28 +121,19 @@ const Home = () => {
                 the rest. My goal is to create an instant positive reaction each
                 and every time I show a client the initial drawing of their new
                 tattoo. With over a decade of experience, I have found the right
-                location for me, at Grey Havens Tattoo. I’m at a shop that lets
-                me express who I am and who I want to be artistically. Here I’m
-                able to really focus on tailoring my experience to each
-                individual client and giving them the best custom tattoo they
-                could ask for.
+                location for me, at Grey Havens Tattoo.
               </p>
             </div>
             <div className={styles.paragraphSection}>
               <h2 className={styles.subtitle}>What we do</h2>
               <p>
-                I pride myself on standing out by being a client driven artist
-                who listens and wants the customer to have a one of a kind
+                I pride myself on standing out by being a client-driven artist
+                who listens and wants the customer to have a one-of-a-kind
                 experience they will always remember. Based on past personal
-                experiences at other shops I realize the process of getting a
-                tattoo can be a very intimidating experience for anyone, so I
-                want them to be comfortable and understand that I’m on the same
-                page, patient and listening. Different from many other artists,
-                I set aside a lot of time to prepare for each client and hold
-                myself to an extremely high standard. I envision every detail of
-                the tattoo they want and create a custom piece that exceeds
-                their expectations. I enjoy implementing aspects of texture into
-                each unique tattoo I create to give it added depth.{" "}
+                experiences at other shops, I realize the process of getting a
+                tattoo can be intimidating for anyone, so I want them to be
+                comfortable and understand that I’m on the same page, patient,
+                and listening.
               </p>
             </div>
           </div>
