e7ba91b1ca335590c11051b77c037fdf56c6de7f
John <ginuwine104@gmail.com>
2024-12-10 23:49:17 -0500

    Added new artist

--- src/components/artist/Artist.jsx
+++ src/components/artist/Artist.jsx
@@ -2,58 +2,72 @@ import React from "react";
 import styles from "./Artist.module.css";
 import artistsData from "../artistsData";
 import Link from "next/link";
-import { motion } from 'framer-motion';
-const Artist = ({ showBioSection = false }) => {
+import { motion } from "framer-motion";
+
+const Artist = () => {
+  const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+      opacity: 1,
+      transition: { staggerChildren: 0.3 },
+    },
+  };
 
   const bioVariants = {
-    hidden: { opacity: 0, x: -100 },
-    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, y: -20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
   };
+
+  const cardVariants = {
+    hidden: { opacity: 0, scale: 0.9 },
+    visible: { opacity: 1, scale: 1, transition: { duration: 0.6 } },
+  };
+
   return (
     <>
       <div className={styles.parallaxBackground} /> {/* Parallax Background */}
       <div className={styles.wholeContainer}>
         <h2 className={`text-center ${styles.artistTitle}`}>Artist</h2>
 
-        {/* Conditionally render artist bios section */}
-        {showBioSection && (
-            <div className={styles.artistBios}>
-            {artistsData.map((artist) => (
-              // Use a unique piece of data for the key, such as an id
-              <motion.div 
-                key={artist.id} 
-                className={styles.artistBio}
-                variants={bioVariants}
-                initial="hidden"
-                animate="visible"
-                transition={{ duration: 0.5, delay: 0.2 * artistsData.indexOf(artist) }}
-              >
+        {/* Artist Cards with Bio */}
+        <motion.div
+          className={styles.artistContainer}
+          variants={containerVariants}
+          initial="hidden"
+          animate="visible"
+        >
+          {artistsData.map((artist) => (
+            <div key={artist.id} className={styles.artistCardWrapper}>
+              {/* Animated Artist Bio */}
+              <motion.div className={styles.artistBio} variants={bioVariants}>
                 {artist.bio}
               </motion.div>
-            ))}
-          </div>
-        )}
 
-        {/* Container for artist cards */}
-        <div className={styles.artistContainer}>
-          {artistsData.map((artist) => (
-            <div key={artist.id} className={styles.artistCard}>
-              <img
-                src={artist.imageUrl}
-                alt={artist.name}
-                className={styles.artistImage}
-              />
-              <div className={styles.artistOverlay}>
-                <h2 className={styles.artistName}>{artist.name}</h2>
-                <div className={styles.buttonContainer}>
-                  <Link href={`/${artist.name}`} passHref>
-                    <div className={styles["round-button"]}>Gallery</div>
-                  </Link>
+              {/* Animated Artist Card */}
+              <motion.div className={styles.artistCard} variants={cardVariants}>
+                <img
+                  src={artist.imageUrl}
+                  alt={artist.name}
+                  className={styles.artistImage}
+                />
+                <div className={styles.artistOverlay}>
+                  <h2 className={styles.artistName}>{artist.name}</h2>
+                  <div className={styles.buttonContainer}>
+                    <Link href={`/${artist.name}`} passHref>
+                      <motion.div
+                        className={styles["round-button"]}
+                        whileHover={{ scale: 1.1 }}
+                        whileTap={{ scale: 0.95 }}
+                      >
+                        Gallery
+                      </motion.div>
+                    </Link>
+                  </div>
                 </div>
-              </div>
+              </motion.div>
             </div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </>
   );
--- src/components/artistsData.jsx
+++ src/components/artistsData.jsx
@@ -8,12 +8,20 @@ const artistsData = [
     imageUrl: "/Assets/d.png", // Image URL for Theron's profile picture
     bio: (
       <>
-      <p>Hello, my name is Theron.</p>
-      <p>I am a new artist and recent apprentice at Inkstaind Tattoo, where I learned everything I know about the profession from Crystal.</p>
-      <p>I grew up in Buckley WA, producing creative drawings from a young age. I graduated from White River with knowledge and passion for art and graphic design, along with several art show awards and publicized pieces of work.</p>
-      <p>Although I have room to flourish in unlimited styles of tattoos, I have a fondness for American Traditional work. I can’t help but gravitate toward bold lines and bright colors. I’m still pushing my limits and expanding my skill set every day.</p>
-    </>
-  ), 
+        <p>Hello, my name is Theron.</p>
+        <p>
+          I am the owner and operator of,  <a
+            href="https://www.instagram.com/theronissac/"
+            style={{ color: "red" }} // Corrected style object
+          >
+            SON OF INK
+          </a>
+          .
+        </p>
+        <p>I grew up in DMV, producing creative drawings from a young age. I have a passion for art and graphic design, along with several art show awards and publicized pieces of work.</p>
+        <p>Although I have room to flourish in unlimited styles of tattoos, I have a fondness for American Traditional work. I’m still pushing my limits and expanding my skill set every day.</p>
+      </>
+    ),
     galleryImages: [
       { url: "/Assets/1.png" },
       { url: "/Assets/2.png" },
@@ -26,15 +34,31 @@ const artistsData = [
   },
   {
     id: 2,
-    name: "Art",
-    imageUrl: "/Assets/tatArt.png", // Image URL for Art profile picture
-    bio: "", 
+    name: "Douglas",
+    imageUrl: "/Assets/douglas.png", // Image URL for Art profile picture
+    bio: (
+      <>
+        <p>Hello, my name Douglas.</p>
+        <p>
+          I am a new artist and recent apprentice at Inkstaind Tattoo,  <a
+            href="https://planetlbx.com/"
+            style={{ color: "red" }} // Corrected style object
+          >
+            PLANET LUNCHBOX
+          </a>
+          .
+        </p>
+        <p>Welcome to the home planet of earth bound Tattoo Artist Doug-E! You can find all his Art and Merch exclusively here!
+          Ready for your next Tattoo? Just click and book your appointment today! </p>
+
+      </>
+    ),
     galleryImages: [
-      { url: "/Assets/d.png" },
-      { url: "/Assets/c.png" },
-      { url: "/Assets/b.png" },
-      { url: "/Assets/a.png" },
-      { url: "/Assets/e.png" },
+      { url: "/Assets/f.png" },
+      { url: "/Assets/g.png" },
+      { url: "/Assets/h.png" },
+      { url: "/Assets/i.png" },
+      { url: "/Assets/j.png" },
     ],
   },
   // ... add more artists with their data
